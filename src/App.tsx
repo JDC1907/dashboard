fff98dff872dd9601aaf71343a7b448e68431cf8
Jahir Alexander Díaz Cedeño <jaaldiaz@espol.edu.ec>
2024-12-02 10:28:54 -0500

    actualizaciones

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Item from '../interface/Item.tsx';
 // Grid version 2
 import Grid from '@mui/material/Grid2'
 import './App.css'
@@ -6,38 +7,125 @@ import TableWeather from './components/TableWeather';
 import ControlWeather from './components/ControlWeather';
 import LineChartWeather from './components/LineChartWeather';
 
+{/* Hooks */ }
+//import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+
+interface Indicator {
+  title?: String;
+  subtitle?: String;
+  value?: String;
+}
+
 function App() {
+
+  {/* Variable de estado y función de actualización */ }
+  let [indicators, setIndicators] = useState<Indicator[]>([])
+
+  {/* Hook: useEffect */ }
+
+
+  {/* Hook: useEffect */ }
+  useEffect(() => {
+
+    let request = async () => {
+
+      {/* Request */ }
+      let API_KEY = "6e4d25d80f777aee491567f2de1118ba"
+      let response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Guayaquil&mode=xml&appid=${API_KEY}`)
+      let savedTextXML = await response.text();
+
+      {/* XML Parser */ }
+      const parser = new DOMParser();
+      const xml = parser.parseFromString(savedTextXML, "application/xml");
+
+      {/* Arreglo para agregar los resultados */ }
+
+      let dataToIndicators: Indicator[] = new Array<Indicator>();
+
+      {/* 
+            Análisis, extracción y almacenamiento del contenido del XML 
+            en el arreglo de resultados
+        */}
+
+      let name = xml.getElementsByTagName("name")[0].innerHTML || ""
+      dataToIndicators.push({ "title": "Location", "subtitle": "City", "value": name })
+
+      let location = xml.getElementsByTagName("location")[1]
+
+      let latitude = location.getAttribute("latitude") || ""
+      dataToIndicators.push({ "title": "Location", "subtitle": "Latitude", "value": latitude })
+
+      let longitude = location.getAttribute("longitude") || ""
+      dataToIndicators.push({ "title": "Location", "subtitle": "Longitude", "value": longitude })
+
+      let altitude = location.getAttribute("altitude") || ""
+      dataToIndicators.push({ "title": "Location", "subtitle": "Altitude", "value": altitude })
+
+      console.log(dataToIndicators)
+
+
+      // console.log( dataToIndicators )
+
+      {/* Modificación de la variable de estado mediante la función de actualización */ }
+      setIndicators(dataToIndicators)
+
+    }
+
+    request();
+  }, [])
+
+
   return (
     <Grid container spacing={5}>
 
       {/* Indicadores */}
-      <Grid size={{ xs: 12, xl: 3 }}><IndicatorWeather title={'Indicator 1'} subtitle={'Unidad 1'} value={"1.23"} />
+      {/* <Grid size={{ xs: 12, sm: 3 }}><IndicatorWeather title={'Indicator 1'} subtitle={'Unidad 1'} value={"1.23"} />
       </Grid>
-      <Grid size={{ xs: 12, xl: 3 }}><IndicatorWeather title={'Indicator 2'} subtitle={'Unidad 2'} value={"3.12"} />
+      <Grid size={{ xs: 12, sm: 3 }}><IndicatorWeather title={'Indicator 2'} subtitle={'Unidad 2'} value={"3.12"} />
       </Grid>
-      <Grid size={{ xs: 12, xl: 3 }}> <IndicatorWeather title={'Indicator 3'} subtitle={'Unidad 3'} value={"2.31"} />
+      <Grid size={{ xs: 12, sm: 3 }}> <IndicatorWeather title={'Indicator 3'} subtitle={'Unidad 3'} value={"2.31"} />
       </Grid>
-      <Grid size={{ xs: 12, xl: 3 }}><IndicatorWeather title={'Indicator 4'} subtitle={'Unidad 4'} value={"3.21"} />
-      </Grid>
+      <Grid size={{ xs: 12, sm: 3 }}><IndicatorWeather title={'Indicator 4'} subtitle={'Unidad 4'} value={"3.21"} />
+      </Grid> */}
+
+      {
+        indicators
+          .map(
+            (indicator, idx) => (
+              <Grid key={idx} size={{ xs: 12, xl: 3 }}>
+                <IndicatorWeather
+                  title={indicator["title"]}
+                  subtitle={indicator["subtitle"]}
+                  value={indicator["value"]} />
+              </Grid>
+            )
+          )
+      }
+
 
       {/* Tabla */}
-      <Grid></Grid>
-      {/* Grid Anidado */}
-      <Grid container spacing={2}>
-        <Grid size={{ xs: 12, xl: 3 }}>
-          <ControlWeather />
-        </Grid>
-        <Grid >
-          <TableWeather />
-        </Grid>
-      </Grid>
+      <Grid size={{ xs: 12, sm: 9 }}>
 
-      {/* Gráfico */}
-      <Grid>
-        <LineChartWeather />
-      </Grid>
 
-    </Grid>
+        {/* Grid Anidado */}
+        <Grid container spacing={2}>
+          <Grid size={{ xs: 12, sm: 3 }}>
+            <ControlWeather />
+          </Grid>
+          <Grid size={{ xs: 12, sm: 9 }}>
+            <TableWeather />
+          </Grid>
+        </Grid>
+
+        {/* Gráfico */}
+        <Grid size={{ xs: 12, sm: 7 }}>
+
+          <LineChartWeather />
+        </Grid>
+
+      </Grid>
+    </Grid >
   )
 }
 
