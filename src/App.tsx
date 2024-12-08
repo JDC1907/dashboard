979bc41d3c79b58cbb5af4531815674c9b9bf6bd
Jahir Alexander Díaz Cedeño <jaaldiaz@espol.edu.ec>
2024-12-08 10:49:05 -0500

    Cambios realizados de la actividad de react

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import Item from '../interface/Item.tsx';
+import Item from './interface/Item'; // Importar la interfaz Item
 // Grid version 2
-import Grid from '@mui/material/Grid2'
-import './App.css'
+import Grid from '@mui/material/Grid2';
+import './App.css';
 import IndicatorWeather from './components/IndicatorWeather';
 import TableWeather from './components/TableWeather';
 import ControlWeather from './components/ControlWeather';
 import LineChartWeather from './components/LineChartWeather';
 
-{/* Hooks */ }
 //import { useEffect } from 'react';
 import { useEffect, useState } from 'react';
 
@@ -20,10 +19,8 @@ interface Indicator {
 function App() {
 
   {/* Variable de estado y función de actualización */ }
-  let [indicators, setIndicators] = useState<Indicator[]>([])
-
-  {/* Hook: useEffect */ }
-
+  let [indicators, setIndicators] = useState<Indicator[]>([]);
+  let [items, setItems] = useState<Item[]>([]); // Variable de estado añadida
 
   {/* Hook: useEffect */ }
   useEffect(() => {
@@ -31,8 +28,8 @@ function App() {
     let request = async () => {
 
       {/* Request */ }
-      let API_KEY = "6e4d25d80f777aee491567f2de1118ba"
-      let response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Guayaquil&mode=xml&appid=${API_KEY}`)
+      let API_KEY = "6e4d25d80f777aee491567f2de1118ba";
+      let response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Guayaquil&mode=xml&appid=${API_KEY}`);
       let savedTextXML = await response.text();
 
       {/* XML Parser */ }
@@ -40,55 +37,60 @@ function App() {
       const xml = parser.parseFromString(savedTextXML, "application/xml");
 
       {/* Arreglo para agregar los resultados */ }
-
       let dataToIndicators: Indicator[] = new Array<Indicator>();
+      let dataToItems: Item[] = []; // Arreglo temporal añadido
 
       {/* 
             Análisis, extracción y almacenamiento del contenido del XML 
             en el arreglo de resultados
         */}
 
-      let name = xml.getElementsByTagName("name")[0].innerHTML || ""
-      dataToIndicators.push({ "title": "Location", "subtitle": "City", "value": name })
+      let name = xml.getElementsByTagName("name")[0].innerHTML || "";
+      dataToIndicators.push({ "title": "Location", "subtitle": "City", "value": name });
 
-      let location = xml.getElementsByTagName("location")[1]
+      let location = xml.getElementsByTagName("location")[1];
 
-      let latitude = location.getAttribute("latitude") || ""
-      dataToIndicators.push({ "title": "Location", "subtitle": "Latitude", "value": latitude })
+      let latitude = location.getAttribute("latitude") || "";
+      dataToIndicators.push({ "title": "Location", "subtitle": "Latitude", "value": latitude });
 
-      let longitude = location.getAttribute("longitude") || ""
-      dataToIndicators.push({ "title": "Location", "subtitle": "Longitude", "value": longitude })
+      let longitude = location.getAttribute("longitude") || "";
+      dataToIndicators.push({ "title": "Location", "subtitle": "Longitude", "value": longitude });
 
-      let altitude = location.getAttribute("altitude") || ""
-      dataToIndicators.push({ "title": "Location", "subtitle": "Altitude", "value": altitude })
+      let altitude = location.getAttribute("altitude") || "";
+      dataToIndicators.push({ "title": "Location", "subtitle": "Altitude", "value": altitude });
 
-      console.log(dataToIndicators)
+      // Extraer datos del XML y almacenarlos en dataToItems
+      let timeNodes = xml.getElementsByTagName("time");
+      for (let i = 0; i < Math.min(6, timeNodes.length); i++) {
+        let timeNode = timeNodes[i];
 
+        let dateStart = timeNode.getAttribute("from") || "";
+        let dateEnd = timeNode.getAttribute("to") || "";
 
-      // console.log( dataToIndicators )
+        let precipitationNode = timeNode.querySelector("precipitation");
+        let precipitation = precipitationNode?.getAttribute("probability") || "";
+
+        let humidityNode = timeNode.querySelector("humidity");
+        let humidity = humidityNode?.getAttribute("value") || "";
+
+        let cloudsNode = timeNode.querySelector("clouds");
+        let clouds = cloudsNode?.getAttribute("all") || "";
+
+        // Crear objeto de tipo Item y agregarlo al arreglo temporal
+        dataToItems.push({ dateStart, dateEnd, precipitation, humidity, clouds });
+      }
 
       {/* Modificación de la variable de estado mediante la función de actualización */ }
-      setIndicators(dataToIndicators)
-
-    }
+      setIndicators(dataToIndicators);
+      setItems(dataToItems); // Actualizar estado con los datos procesados
+    };
 
     request();
-  }, [])
-
+  }, []);
 
   return (
     <Grid container spacing={5}>
-
       {/* Indicadores */}
-      {/* <Grid size={{ xs: 12, sm: 3 }}><IndicatorWeather title={'Indicator 1'} subtitle={'Unidad 1'} value={"1.23"} />
-      </Grid>
-      <Grid size={{ xs: 12, sm: 3 }}><IndicatorWeather title={'Indicator 2'} subtitle={'Unidad 2'} value={"3.12"} />
-      </Grid>
-      <Grid size={{ xs: 12, sm: 3 }}> <IndicatorWeather title={'Indicator 3'} subtitle={'Unidad 3'} value={"2.31"} />
-      </Grid>
-      <Grid size={{ xs: 12, sm: 3 }}><IndicatorWeather title={'Indicator 4'} subtitle={'Unidad 4'} value={"3.21"} />
-      </Grid> */}
-
       {
         indicators
           .map(
@@ -114,7 +116,7 @@ function App() {
             <ControlWeather />
           </Grid>
           <Grid size={{ xs: 12, sm: 9 }}>
-            <TableWeather />
+          <TableWeather itemsIn={items} />
           </Grid>
         </Grid>
 
@@ -126,7 +128,7 @@ function App() {
 
       </Grid>
     </Grid >
-  )
+  );
 }
 
-export default App
+export default App;
